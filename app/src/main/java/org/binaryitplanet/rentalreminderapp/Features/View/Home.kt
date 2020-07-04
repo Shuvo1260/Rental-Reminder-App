06deef2df3aae7b3113bf4332565ffbc771f0351
Shuvo <shovocse753@gmail.com>
2020-07-05 01:57:22 +0600

    List item views are ready.

--- app/src/main/java/org/binaryitplanet/rentalreminderapp/Features/View/Home.kt
+++ app/src/main/java/org/binaryitplanet/rentalreminderapp/Features/View/Home.kt
@@ -6,16 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.binaryitplanet.rentalreminderapp.R
+import org.binaryitplanet.rentalreminderapp.databinding.FragmentHomeBinding
 
 
 class Home : Fragment() {
+
+    private val TAG = "Home"
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.add.setOnClickListener {
+            //
+        }
+
+        return binding.root
     }
 
 }

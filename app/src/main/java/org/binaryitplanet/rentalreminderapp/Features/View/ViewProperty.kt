1ffc9a798b7797ad94c55559e343ec93dc1ef4b9
Shuvo <shovocse753@gmail.com>
2020-07-05 03:01:55 +0600

    Home, Old tenant, Property view screen is complete and reminder service class is created.

---
+++ app/src/main/java/org/binaryitplanet/rentalreminderapp/Features/View/ViewProperty.kt
@@ -0,0 +1,12 @@
+package org.binaryitplanet.rentalreminderapp.Features.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.binaryitplanet.rentalreminderapp.R
+
+class ViewProperty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_property)
+    }
+}
